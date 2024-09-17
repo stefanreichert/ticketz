f2fdc2c888dc95620d8783ff13db677579ff4dd3
stefan <stefan@wickedshell.net>
2024-09-17 13:57:43 +0200

    add custom validation for password confirm

--- src/main/java/net/wickedshell/ticketz/adapter/web/controller/SignupController.java
+++ src/main/java/net/wickedshell/ticketz/adapter/web/controller/SignupController.java
@@ -1,5 +1,6 @@
 package net.wickedshell.ticketz.adapter.web.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.wickedshell.ticketz.adapter.web.WebAction;
 import net.wickedshell.ticketz.adapter.web.WebView;
@@ -9,6 +10,7 @@ import net.wickedshell.ticketz.service.model.User;
 import net.wickedshell.ticketz.service.port.rest.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,12 +31,22 @@ public class SignupController {
     }
 
     @PostMapping(WebAction.ACTION_SIGNUP)
-    public ModelAndView signup(@ModelAttribute Signup signup) {
+    public ModelAndView signup(@Valid @ModelAttribute Signup signup, BindingResult bindingResult) {
+        if (validationFailed(signup, bindingResult)) {
+            return new ModelAndView(WebView.VIEW_SIGNUP);
+        }
         User user = new User();
         user.setFirstname(signup.getFirstname());
         user.setLastname(signup.getLastname());
         user.setEmail(signup.getEmail());
         userService.create(user, signup.getPassword(), Set.of(Role.ROLE_USER));
         return new ModelAndView("redirect:" + WebAction.ACTION_SHOW_LOGIN);
+    }
+
+    private static boolean validationFailed(Signup signup, BindingResult bindingResult) {
+        if (!signup.getPassword().equals(signup.getConfirmPassword())) {
+            bindingResult.rejectValue("confirmPassword","signup.password.mismatch", "Passwords do not match");
+        }
+        return bindingResult.hasErrors();
     }
 }
